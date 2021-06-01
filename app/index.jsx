bce94a828c36deeee94f3da5cbff57c1ce979cfa
lot9s <lordofthenines@gmail.com>
2021-06-01 13:56:37 -0700

    added menu stub

--- app/index.jsx
+++ app/index.jsx
@@ -14,7 +14,29 @@ class App extends React.Component {
     
     render() {
         return (
-          <div className='container-fluid h-100 p-0'></div>
+          <div className='container-fluid h-100 p-0'>
+            <nav className='navbar navbar-expand-sm navbar-dark bg-dark'>
+              <div className='container-fluid'>
+                <a className='navbar-brand' href='#'>999</a>
+          
+                <button className='navbar-toggler' type='button' data-bs-toggle='collapse' data-bs-target='#menuText' aria-controls='menuText' aria-expanded='false' aria-label='Toggle menu.'>
+                  <span className='navbar-toggler-icon'></span>
+                </button>
+              </div>
+          
+              <div className='collapse navbar-collapse me-auto' id='menuText'>
+                <ul className='navbar-nav'>
+                  <li className='nav-item mx-2'>
+                    <a className="nav-link" href="#">Save</a>
+                  </li>
+        
+                  <li className='nav-item mx-2'>
+                    <a className="nav-link" href="#">Load</a>
+                  </li>
+                </ul>
+              </div>
+            </nav>
+          </div>
         );
     }
 }
