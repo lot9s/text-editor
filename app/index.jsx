d18e033b3548170886492b277bb06376305fad5a
Pablo Ortiz <lot9s@users.noreply.github.com>
2021-06-04 10:45:18 -0700

    save+load items visible iff browser has file system access api compatibility

--- app/index.jsx
+++ app/index.jsx
@@ -9,37 +9,59 @@ class App extends React.Component {
     constructor(props) {
         super(props);
 
-        this.state = {};
+        this.state = {
+          canAccessFS: 'chooseFileSystemEntries' in window || 'showOpenFilePicker' in window
+        };
+    }
+
+    onLoad() {
+      /* TODO: */  
+    }
+
+    onSave() {
+      /* TODO: */
     }
     
     render() {
-        return (
-          <div className='container-fluid h-100 p-0'>
-            <nav className='navbar navbar-expand-sm navbar-dark bg-dark'>
-              <div className='container-fluid'>
-                <a className='navbar-brand' href='#'>999</a>
-          
-                <button className='navbar-toggler' type='button' data-bs-toggle='collapse' data-bs-target='#menuText' aria-controls='menuText' aria-expanded='false' aria-label='Toggle menu.'>
-                  <span className='navbar-toggler-icon'></span>
-                </button>
-              </div>
-          
-              <div className='collapse navbar-collapse me-auto' id='menuText'>
-                <ul className='navbar-nav'>
-                  <li className='nav-item mx-2'>
-                    <a className="nav-link" href="#">Save</a>
-                  </li>
-        
-                  <li className='nav-item mx-2'>
-                    <a className="nav-link" href="#">Load</a>
-                  </li>
-                </ul>
-              </div>
-            </nav>
-          
-            <textarea id='editor' className='h-100 w-100'></textarea>
+      let ioMenu = null;
+      let ioMenuItems = null;
+      if (this.state.canAccessFS) {
+        ioMenu = (
+          <button className='navbar-toggler' type='button' data-bs-toggle='collapse' data-bs-target='#menuText' aria-controls='menuText' aria-expanded='false' aria-label='Toggle menu.'>
+            <span className='navbar-toggler-icon'></span>
+          </button>
+        );
+
+        ioMenuItems = (
+          <div className='collapse navbar-collapse me-auto' id='menuText'>
+            <ul className='navbar-nav'>
+              <li className='nav-item mx-2'>
+                <a className="nav-link" href="#">Save</a>
+              </li>
+    
+              <li className='nav-item mx-2'>
+                <a className="nav-link" href="#">Load</a>
+              </li>
+            </ul>
           </div>
         );
+      }
+
+      return (
+        <div className='container-fluid h-100 p-0'>
+          <nav className='navbar navbar-expand-sm navbar-dark bg-dark'>
+            <div className='container-fluid'>
+              <a className='navbar-brand' href='#'>999</a>
+        
+              {ioMenu}
+            </div>
+
+            {ioMenuItems}
+          </nav>
+        
+          <textarea id='editor' className='h-100 w-100'></textarea>
+        </div>
+      );
     }
 }
 
